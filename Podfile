dc0e24d58e8c961d3efa2b42a354afd272ae8c2c
Minh Tu Le <minhtule05@gmail.com>
2015-07-28 20:05:33 +0700

    Update to Alamofire 1.3.0 and PredictionIOSDK 0.2.0

--- Podfile
+++ Podfile
@@ -4,4 +4,4 @@ use_frameworks!
 
 pod 'Alamofire', '~> 1.2'
 pod 'ObjectMapper', :git => 'https://github.com/Hearst-DD/ObjectMapper.git', :branch => 'swift-1.2'
-pod 'PredictionIOSDK', :git => 'https://github.com/minhtule/PredictionIO-Swift-SDK.git', :branch => 'swift-1.2' 
+pod 'PredictionIOSDK', :git => 'https://github.com/minhtule/PredictionIO-Swift-SDK.git'
--- Tapster iOS Demo/Alamofire+UIImage.swift
+++ Tapster iOS Demo/Alamofire+UIImage.swift
@@ -9,21 +9,18 @@
 import Alamofire
 
 extension Alamofire.Request {
-    class func imageResponseSerializer() -> Serializer {
-        return { request, response, data in
+    class func imageResponseSerializer() -> GenericResponseSerializer<UIImage> {
+        return GenericResponseSerializer<UIImage> { request, response, data in
             if data == nil {
                 return (nil, nil)
             }
             
             let image = UIImage(data: data!, scale: UIScreen.mainScreen().scale)
-            
             return (image, nil)
         }
     }
     
     func responseImage(completionHandler: (NSURLRequest, NSHTTPURLResponse?, UIImage?, NSError?) -> Void) -> Self {
-        return response(serializer: Request.imageResponseSerializer(), completionHandler: { (request, response, image, error) in
-            completionHandler(request, response, image as? UIImage, error)
-        })
+        return response(responseSerializer: Request.imageResponseSerializer(), completionHandler: completionHandler)
     }
 }
