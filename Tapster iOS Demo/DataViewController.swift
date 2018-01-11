172d03019b138a18dc98fb380b1f9883b302e2d6
Minh-Tu Le <minhtule05@gmail.com>
2018-01-11 21:01:52 +0800

    Use batch events to import data. This reduces the memory usage and run much faster.

--- Tapster iOS Demo/DataViewController.swift
+++ Tapster iOS Demo/DataViewController.swift
@@ -10,13 +10,8 @@ import UIKit
 import PredictionIO
 
 class DataViewController: UIViewController {
-    let userIDColumn = 0
-    let episodeIDColumn = 1
     let eventClient = EventClient(accessKey: "")
-    let numberOfConcurrentRequests = 4 // Same as Apple's HTTPMaximumConnectionsPerHost default value for iOS
-    var globalLikeIndex = 0
-    var finishingCounter = 0
-    
+
     @IBAction func importDataButtonAction(_ sender: UIButton) {
         self.readCSVDataAndImport()
     }
@@ -28,56 +23,36 @@ class DataViewController: UIViewController {
         
         print("Found \(episodeData.rows.count) unique episode")
         print("Sending episodes data...")
-        
-        for row in episodeData.rows {
+
+        processRows(episodeData.rows, messagePrefix: "Sending episode") { row in
             let properties: [String: Any] = [
                 "title": row[1],
                 "categories": convertCategories(row[2]),
                 "imageURLs": convertImageURLs(row[4]),
-            ]
-            eventClient.setItem(itemID: row[0], properties: properties) { data, error in
-                // Only report if an error occurs
-                if let error = error {
-                    print("Resonse \(String(describing: data))")
-                    print("Sending episode: \(row[0]) has error \(error)")
-                }
-            }
+                ]
+            return Event(event: Event.setEvent, entityType: Event.itemEntityType, entityID: row[0], properties: properties)
         }
-        
-        print("Done with episodes!")
-        
+
         // MARK: - users data
         
         let likesData = CSVData(fileName: "user_list")
-        let userIDs = likesData.uniqueEntries(column: userIDColumn)
+        let userIDs = likesData.uniqueEntries(column: 0)
         
         print("Found \(userIDs.count) unique user IDs")
         print("Sending users data...")
-        
-        for userID in userIDs {
-            eventClient.setUser(userID: userID, properties: [:]) { data, error in
-                // Only report if an error occurs
-                if let error = error {
-                    print("Resonse \(String(describing: data))")
-                    print("Sending user: \(userID) has error \(error)")
-                }
-            }
+
+        let userIDRows = userIDs.map { [$0] }
+        processRows(userIDRows, messagePrefix: "Sending user") { row in
+            return Event(event: Event.setEvent, entityType: Event.userEntityType, entityID: row[0])
         }
-        
-        print("Done with users!")
-        
+
         // MARK: - likes data
         
         print("Sending likes data...")
-        
-        let startTime = Date()
-        print("Start time = \(startTime)")
 
-        globalLikeIndex = numberOfConcurrentRequests
-        for i in 0..<numberOfConcurrentRequests {
-            sendLikeData(likesData, rowIndex: i)
+        processRows(likesData.rows, messagePrefix: "Sending likes") { row in
+            return Event(event: "like", entityType: Event.userEntityType, entityID: row[0], targetEntity: (Event.itemEntityType, row[1]))
         }
-        
     }
     
     private func convertCategories(_ categories: String) -> [String] {
@@ -87,24 +62,34 @@ class DataViewController: UIViewController {
     private func convertImageURLs(_ imageURLs: String) -> [String] {
         return imageURLs.components(separatedBy: CharacterSet(charactersIn: ";"))
     }
-    
-    private func sendLikeData(_ likesData: CSVData, rowIndex: Int) {
-        let row = likesData.rows[rowIndex]
-        eventClient.recordAction("like", byUserID: row[0], onItemID: row[1]) { _, error in
-            if let error = error {
-                print("Sending like for user \(row[0]) and item \(row[1]) has error \(error)")
-            }
 
-            if self.globalLikeIndex < likesData.rows.count {
-                self.sendLikeData(likesData, rowIndex: self.globalLikeIndex)
-                self.globalLikeIndex += 1
-            } else if self.globalLikeIndex == likesData.rows.count {
-                self.finishingCounter += 1
-                if self.finishingCounter == self.numberOfConcurrentRequests {
-                    let endTime = Date()
-                    print("End time = \(endTime)")
+    private func processRows(_ rows: [[String]], messagePrefix: String, transformRow: ([String]) -> Event) {
+        let batchSize = 50
+        let total = rows.count
+        var currentOffset = 0
+        var nextOffset = 0
+
+        while currentOffset < total {
+            nextOffset = min(currentOffset + batchSize, total)
+
+            let events = rows[currentOffset..<nextOffset].map(transformRow)
+            eventClient.createBatchEvents(events) { [numProcessed = nextOffset] statuses, error in
+                if let error = error {
+                    print("Cannot create event due to \(error)")
+                }
+
+                if let statuses = statuses {
+                    for case let .failed(message) in statuses {
+                        print("Cannot create event due to error \(message)")
+                    }
+                }
+
+                if numProcessed % 500 == 0 || numProcessed == total {
+                    print("\(messagePrefix) - Sent \(numProcessed)/\(total) events.")
                 }
             }
+
+            currentOffset = nextOffset
         }
     }
 }
